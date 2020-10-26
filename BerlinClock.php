9c5bf58fb445365a10b9039375ac73a8878b8bd0
Dorian <dorian.fouvez@student.vinci.be>
2020-10-26 22:16:22 +0100

    refactor - manage multiple of 5 with a modula

--- BerlinClock.php
+++ BerlinClock.php
@@ -4,11 +4,12 @@
 class BerlinClock{
 
     public function countSimpleMinutes(int $int): string{
+        $int = $int%5;
+
         if($int === 1) return "1";
         if($int === 2) return "2";
         if($int === 3) return "3";
         if($int === 4) return "4";
-        if($int === 5) return "0";
 
         return "0";
     }
