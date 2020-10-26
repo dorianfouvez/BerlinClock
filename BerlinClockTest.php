5faafe96c080a5195a1d73d769de0b98dd5c6543
Dorian <dorian.fouvez@student.vinci.be>
2020-10-26 21:45:26 +0100

    Add Test 1

--- BerlinClockTest.php
+++ BerlinClockTest.php
@@ -1,7 +1,17 @@
 <?php
 
+namespace BerlinClock;
+require "BerlinClock.php";
 
-class BerlinClockTest extends PHPUnit_Framework_TestCase
-{
+use PHPUnit\Framework\TestCase;
 
+class BerlinClockTest extends TestCase{
+
+    public function test_count_given1_shouldReturn1(){
+        $berlin = new BerlinClock();
+
+        $actual = $berlin->count(1);
+
+        $this->assertEquals("1", $actual);
+    }
 }
